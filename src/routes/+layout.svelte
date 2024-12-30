49c42b87e450bf9d7e02e77e193f8b3f2fc99c8d
Insub Kim <kegorii@gmail.com>
2024-12-30 20:12:32 +0900

    vercel speed insight, Analytics 적용

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import '../app.css';
+    import { injectAnalytics } from '@vercel/analytics/sveltekit'
 	let { children } = $props();
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+    import { dev } from '$app/environment';
+
+    injectSpeedInsights();
+    injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 {@render children()}
